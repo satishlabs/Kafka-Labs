ca74f675af678b334507a00014d2056a0bac5444
Satish Prasad <spd61185@gmail.com>
2021-02-08 16:26:03 +0530

    Updated Kafka Producer

--- MyKafkaProducer/src/main/java/com/kafkaproducer/controller/TestController.java
+++ MyKafkaProducer/src/main/java/com/kafkaproducer/controller/TestController.java
@@ -1,8 +1,22 @@
 package com.kafkaproducer.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.kafkaproducer.service.TestService;
 
 @RestController
 public class TestController {
-
+	
+	@Autowired
+	private TestService testService;
+	
+	@GetMapping("/testMsg")
+	public String testMessage() {
+		System.out.println("TestController --  testMessage()");
+		testService.sendMessage("Hello Guys - This is message1, how kafka is usefull in jms?");
+		//testService.sendMessage("Hello Guys - This is message2");
+		return "String message sent to Kafka Server";
+	}
 }
--- MyKafkaProducer/src/main/java/com/kafkaproducer/service/TestService.java
+++ MyKafkaProducer/src/main/java/com/kafkaproducer/service/TestService.java
@@ -1,5 +1,17 @@
 package com.kafkaproducer.service;
 
-public class TestService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
+@Service
+public class TestService {
+	
+	@Autowired
+	private KafkaTemplate<String, String> strinKafkaTemplate;
+	
+	public void sendMessage(String message) {
+		System.out.println("TestService -- sendMessage()");
+		strinKafkaTemplate.send("myjlctopic", message);
+	}
 }
