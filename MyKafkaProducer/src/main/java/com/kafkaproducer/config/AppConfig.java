c4bdac330406d48e94900739b97de35f5fb0829f
Satish Prasad <spd61185@gmail.com>
2021-02-10 07:18:38 +0530

    Updated KafakProducer

--- MyKafkaProducer/src/main/java/com/kafkaproducer/config/AppConfig.java
+++ MyKafkaProducer/src/main/java/com/kafkaproducer/config/AppConfig.java
@@ -31,25 +31,5 @@ public class AppConfig {
 	public KafkaTemplate<String, String> stringKafkaTemplate(){
 		return new KafkaTemplate<String, String>(stringProducerFactory());
 	}
-	
-	/*
-	 * public ProducerFactory<String, Order> orderProducerFactory(){ Map<String,
-	 * Object> props = new HashMap<String, Object>();
-	 * 
-	 * props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-	 * props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-	 * StringSerializer.class);
-	 * props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-	 * StringSerializer.class);
-	 * 
-	 * return new DefaultKafkaProducerFactory<String, Order>(props); }
-	 */
-	
-	
-	
-	/*
-	 * @Bean(name = "orderKafkaTemplate") public KafkaTemplate<String, Order>
-	 * orderKafkaTemplate(){ return new KafkaTemplate<String,
-	 * Order>(orderProducerFactory()); }
-	 */
+
 }
